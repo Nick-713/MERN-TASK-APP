f118889ec3e2ef3204d376c0d47031d1c6ac88e9
Nick <pereiranicolas713@gmail.com>
2021-11-05 10:58:07 -0300

    funcionalidade de visualizar tarefa ok

--- todoapp/back-end/routes/taskRoutes.js
+++ todoapp/back-end/routes/taskRoutes.js
@@ -3,25 +3,30 @@ const router = express.Router();
 
 const db = require('../services/connection');
 
-// Rota de visualização de tarefas 
-router.get('/list', (req, res) => {
-  res.send('Chegou na rota de lista de tarefas...');
+db.connectToDB(() => {
+  // Rota de visualização de tarefas 
+  router.get('/list',  async (req, res) => {
+    const response = await db.findDocuments()
+    res.send(response);
+  });
+  
+  // Rota para adicionar tarefas 
+  router.post('/add', (req, res) => {
+    res.send('Chegou na rota de adicionar tarefas...');
+  });
+  
+  // Rota de Atualização de tarefas
+  
+  router.patch('/update', (req, res) => {
+    res.send('Chegou na rota de atualizar tarefas...');
+  });
+  
+  // Rota de deleta tarefas 
+  router.delete('/delete', (req, res) => {
+    res.send('Chegou na rota de remover tarefa...');
+  });
+
 });
 
-// Rota para adicionar tarefas 
-router.post('/add', (req, res) => {
-  res.send('Chegou na rota de adicionar tarefas...');
-});
-
-// Rota de Atualização de tarefas
-
-router.patch('/update', (req, res) => {
-  res.send('Chegou na rota de atualizar tarefas...');
-});
-
-// Rota de deleta tarefas 
-router.delete('/delete', (req, res) => {
-  res.send('Chegou na rota de remover tarefa...');
-});
 
 module.exports = router;
